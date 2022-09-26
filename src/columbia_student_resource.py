9bb0b158df0be87819e7c710f6b1fbab754c2fed
Haohua Su <hs3225@columbi.edu>
2022-09-26 19:06:38 -0400

    Use remote DB

--- src/columbia_student_resource.py
+++ src/columbia_student_resource.py
@@ -11,9 +11,9 @@ class ColumbiaStudentResource:
     @staticmethod
     def _get_connection():
 
-        usr = "root"
+        usr = "admin"
         pw = "suhaohua520"
-        h = "localhost"
+        h = "comse6156-hw0-instance.cpes1g4l3sdp.us-east-2.rds.amazonaws.com"
 
         conn = pymysql.connect(
             user=usr,
@@ -27,7 +27,7 @@ class ColumbiaStudentResource:
     @staticmethod
     def get_by_key(key):
 
-        sql = "SELECT * FROM f22_databases.columbia_students where guid=%s";
+        sql = "SELECT * FROM comse6156_hw0_db.columbia_student where uni=%s";
         conn = ColumbiaStudentResource._get_connection()
         cur = conn.cursor()
         res = cur.execute(sql, args=key)
